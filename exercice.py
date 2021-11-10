c18ddd711ba9e81bdea2a297b22b2986cf9e9738
Selamet53 <selamet.zamanoglu@polymtl.ca>
2021-11-09 19:40:57 -0500

    commit 1

--- exercice.py
+++ exercice.py
@@ -2,12 +2,21 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
+import os
+import pandas as pd
 
 
 # TODO: Définissez vos fonctions ici
+def lire_fichier():
+    return pd.read_csv(r'C:\Users\selam\Documents\GitHub\c04-ch10-2-exercices-Selamet53\data\winequality-white.csv', delimiter=";")
 
+
+def trouver_attribut_cible():
+    df = lire_fichier()
+    return df["quality"]
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-    pass
+    #print(lire_fichier())
+    print(trouver_attribut_cible())
